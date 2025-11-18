2e627cc175645e947e6f00e7f3da6b98c2f7f26a
Md Iftekharul Alam <68458610+iftekhar08019@users.noreply.github.com>
2025-11-18 12:04:46 +0100

    problem 4

--- index.ts
+++ index.ts
@@ -34,9 +34,14 @@ class Person {
   }
 }
 
-const person1 = new Person('John Doe', 30);
-console.log(person1.getDetails());
+interface RatedItem {
+  title: string;
+  rating: number;
+}
 
-const person2 = new Person('Alice', 25);
-console.log(person2.getDetails());
+function filterByRating(items: RatedItem[]): RatedItem[] {
+  return items.filter(item => item.rating >= 4);
+}
+
+
 
