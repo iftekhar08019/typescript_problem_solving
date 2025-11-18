66dfae0c448a154bcd438d3eb5632fd228da0cc1
Md Iftekharul Alam <68458610+iftekhar08019@users.noreply.github.com>
2025-11-18 12:33:20 +0100

    problem 5

--- index.ts
+++ index.ts
@@ -43,5 +43,16 @@ function filterByRating(items: RatedItem[]): RatedItem[] {
   return items.filter(item => item.rating >= 4);
 }
 
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  isActive: boolean;
+}
+
+function filterActiveUsers(users: User[]): User[] {
+  return users.filter(user => user.isActive === true);
+}
+
 
 
